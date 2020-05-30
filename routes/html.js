96a2ec40d5534118b25bfb06faa4bf4f2f3c4d4c
Fer Alva <feralvaa@hotmail.com>
2020-05-30 17:16:27 -0400

    third

--- routes/html.js
+++ routes/html.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
 const path = require("path");
 
+app.get("/", function(req, res) {
+  res.json(path.join(__dirname, "public/index.html"));
+});
+
 router.get("/exercise", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
